1d0f13c8092a216c1cf9ccab35b4ed6ad12c3f55
ziad <ziad@yotech.ltd>
2026-04-13 15:30:37 +0600

    Update MainActivity.kt and deploymentTargetSelector.xml

--- app/src/main/java/com/ziad/mappe/library/MainActivity.kt
+++ app/src/main/java/com/ziad/mappe/library/MainActivity.kt
@@ -12,6 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
+
     }
 }
